36e483ce9b04f7908c34ff4e395556cab81d54c2
QiuChenly <QiuChenly@outlook.com>
2023-07-02 00:20:04 +0800

    璃月我的璃月赐我一个璃月吧求求你了mihoyo🥺我真的测你吗

---
+++ tool/surge.sh
@@ -0,0 +1,3 @@
+xattr -c ~/Library/Application\ Support/com.nssurge.surge-mac
+sudo rm -rf ~/Library/Application Support/com.nssurge.surge-mac
+sudo rm -rf ~/Library/Preferences/com.nssurge.surge-mac.plist
